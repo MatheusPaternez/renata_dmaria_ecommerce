a617612b1b453487a791e674b82890161e5ae5d8
Matheus Paternez <matheusgfpaternez2006@gmail.com>
2026-02-03 12:15:06 -0300

    Redo Cache for store/views

--- store/views.py
+++ store/views.py
@@ -2,18 +2,11 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 from django.views.decorators.cache import cache_page
 
+@cache_page(60 * 5)
 def product_all(request):
-    # Pega todos os produtos ativos
     products = Product.objects.filter(is_active=True)
     return render(request, 'store/index.html', {'products': products})
 
 def product_detail(request, slug):
-    # Pega um produto específico pelo slug ou retorna erro 404 se não existir
     product = get_object_or_404(Product, slug=slug, in_stock=True)
     return render(request, 'store/single.html', {'product': product})
-
-# Cache por 15 min
-@cache_page(60 * 15)
-def product_all(request):
-    products = Product.objects.filter(is_active=True)
-    return render(request, 'store/index.html', {'products': products})
